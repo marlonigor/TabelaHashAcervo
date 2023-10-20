e78de62856a40f6766e680aacbc31828ec433a0f
marlonigor <66376336+marlonigor@users.noreply.github.com>
2023-10-20 23:51:07 +0000

    structure

--- .ccls-cache/@home@runner@TabelaHashAcervo/structures.h
+++ .ccls-cache/@home@runner@TabelaHashAcervo/structures.h
@@ -1,7 +1,17 @@
 #ifdef structures_h
+#define structures_h
 
 #define tam 250
 
-struct exemplares 
+struct exemplares {
+  int codigo;
+  char titulo[100];
+  char autor[41];
+  char area[7];
+  struct exemplares* prox;
+};
+
+typedef struct exemplares Acervo;
+typedef Acervo* Hash[tam];
 
 #endif
---
+++ functions.c
@@ -0,0 +1,7 @@
+#include "structures.h"
+
+int funcao_Esp(int codigo){
+  return (codigo % tam);
+}
+
+Acervo* insere_Esp(Hash tab, int codigo, char* 
