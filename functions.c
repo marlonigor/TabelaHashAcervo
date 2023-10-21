aa28853d3d2a806f69ad7ea182431f999d0f1078
marlonigor <66376336+marlonigor@users.noreply.github.com>
2023-10-21 00:23:08 +0000

    some functions

--- functions.c
+++ functions.c
@@ -4,4 +4,43 @@ int funcao_Esp(int codigo){
   return (codigo % tam);
 }
 
-Acervo* insere_Esp(Hash tab, int codigo, char* 
+Acervo* insere_Esp(Hash tab, int codigo, char* titulo, char* autor, char* area){
+  int h = funcao_Esp(codigo);
+  
+  Acervo* novo =(Acervo*)malloc(sizeof(Acervo));
+  novo->codigo = codigo;
+  strcpy(novo->titulo, titulo);
+  strcpy(novo->autor, autor);
+  strcpy(novo->area, area);
+  novo->prox = NULL;
+
+  if(tab[h] == NULL){
+    tab[h] = novo;
+  } else {
+    Acervo* atual = tab[h];
+    while(atual->prox != NULL){
+      atual = atual->prox;  
+    }
+    atual->prox = novo;
+  }
+  return novo;
+  
+}
+
+void remove_Esp(Hash tab, int codigo){
+  int h = funcao_Esp(codigo);
+
+  Acervo* atual = tab[h];
+  Acervo* anterior = NULL;
+
+  while(atual != NULL && atual->codigo != codigo){
+    anterior = atual;
+    atual = atual->prox;
+  }
+
+  if(atual != NULL){
+    if (anterior == NULL){
+      tab[h] = atual->prox; 
+    }
+  }
+}
